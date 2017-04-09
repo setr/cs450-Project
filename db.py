8a1384ad32870810e0353f5a73d033e8d59af8aa
Neilabh Okhandiar <setr@dhcp187.merunorth105.iit.edu>
2017-04-08 19:25:52 -0500

    added example login/security table view

--- db.py
+++ db.py
@@ -1,5 +1,7 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.sql import text
+from collections import namedtuple
 
 
 engine = sqlalchemy.create_engine('mysql+pymysql://root:password@cs425.cdm3zyogivv9.us-west-2.rds.amazonaws.com:3306/library')
@@ -8,7 +10,14 @@ def getAllSecurity():
     Session = scoped_session(sessionmaker(bind=engine))
     s = Session()
     result = s.execute('SELECT * FROM Security').fetchall()
-    print result
     return result
 
-getAllSecurity()
+def login(user, passw):
+    Session = scoped_session(sessionmaker(bind=engine))
+    s = Session()
+    q = text("SELECT username, password, securitylevel from Account where username = :user and password = :passw")
+    result = s.execute(q.bindparams(user=user, passw=passw)).fetchone()
+    return result
+
+print login('keshav', '1211')
+#getAllSecurity()
--- init.py
+++ init.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask import url_for, redirect, session
+
+import db
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/security')
+def sec():
+    data = db.getAllSecurity()
+    return render_template('secview.html', data=data)
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = request.form.get('username')
+    passw = request.form.get('password')
+    if not user or not passw:
+        raise BadInput('Missing Username or Password')
+
+    data = db.login(user, passw)
+    if not data:
+        raise BadInput('Incorrect Username/Password')
+    return render_template('index.html', data=data)
