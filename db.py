9d5cf2403be057c304a53499677919e83af20206
Neilabh Okhandiar <setr@dhcp100.merusouth112.iit.edu>
2017-04-09 20:09:48 -0500

    added some template pages and login auth systems

--- db.py
+++ db.py
@@ -2,9 +2,40 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.sql import text
 from collections import namedtuple
+import pymysql
 
+#engine = sqlalchemy.create_engine('mysql+pymysql://root:password@cs425.cdm3zyogivv9.us-west-2.rds.amazonaws.com:3306/library')
+connection = pymysql.connect(
+    user='root',
+    password='password',
+    host='cs425.cdm3zyogivv9.us-west-2.rds.amazonaws.com',
+    port=3306,
+    db='library',
+    cursorclass=pymysql.cursors.DictCursor)
 
-engine = sqlalchemy.create_engine('mysql+pymysql://root:password@cs425.cdm3zyogivv9.us-west-2.rds.amazonaws.com:3306/library')
+def select(query, data, many):
+    result = None
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query, data)
+            if many:
+                result = cursor.fetchall()
+            else:
+                result = cursor.fetchone()
+    finally:
+        pass
+    return result
+    #finally:
+    #    connection.close()
+
+def CUD(queries, datas):
+    try:
+        with connection.cursor() as cursor:
+            for query, data in zip(queries, datas):
+                cursor.execute(query, data)
+        connection.commit()
+    finally:
+        connection.close()
 
 def getAllSecurity():
     Session = scoped_session(sessionmaker(bind=engine))
@@ -13,11 +44,19 @@ def getAllSecurity():
     return result
 
 def login(user, passw):
-    Session = scoped_session(sessionmaker(bind=engine))
-    s = Session()
-    q = text("SELECT username, password, securitylevel from Account where username = :user and password = :passw")
-    result = s.execute(q.bindparams(user=user, passw=passw)).fetchone()
+    #q = "SELECT username, levelname from Account join Security on Account.SecurityLevel = Security.SecurityLevel where username = %s and password = %s"
+    q = "call sp_credcheck(%s,%s)"
+    print q % (user, passw)
+    result = select(query=q, data=(user, passw), many=False)
+    print result
     return result
+            
+    #Session = scoped_session(sessionmaker(bind=engine))
+    #s = Session()
+    #q = text("SELECT username, levelname from Account join Security on Account.SecurityLevel = Security.SecurityLevel where username = :user and password = :passw")
+    #result = s.execute(q.bindparams(user=user, passw=passw)).fetchone()
+    #return result
 
+    #result = s.execute(q.bindparams(user=user, passw=passw)).fetchone()
 print login('keshav', '1211')
 #getAllSecurity()
--- init.py
+++ init.py
@@ -1,26 +1,122 @@
 from flask import Flask, request, render_template
-from flask import url_for, redirect, session
+from flask import url_for, redirect, session, Response
+from flask_wtf import Form
+from wtforms import StringField, validators
+from enum import IntEnum
+from functools import wraps
 
+import errors as err
 import db
+
 app = Flask(__name__)
+app.config['WTF_CSRF_ENABLED'] = False
+
+app.jinja_env.line_statement_prefix = '#'  # enables jinja2 line mode
+app.jinja_env.line_comment_prefix = '##'  # enables jinja2 line mode
+app.secret_key = 'Bd\xf2\x14\xbbi\x01Gq\xc6\x87\x10BVc\x9c\xa4\x08\xdbk%\xfa*\xe3'  # os.urandom(24)
+
+class Role(IntEnum):
+    ADMIN = 1
+    PROFESSOR = 2
+    STUDENT = 3
+    PARENT = 4
+
+def requires_roles(*roles):
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            if not 'role' in session:
+                return Response(
+                    'Could not verify your access level for that URL.\n'
+                    'You have to login with proper credentials', 401,
+                    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+            if session['role'] not in roles:
+                raise err.Forbidden('You do not have permission to access this page'
+                                    )  # proper would be 401 Unauthorized
+            return f(*args, **kwargs)
+        return wrapped
+    return wrapper
+
+def adminonly(fn):
+    """ Decorator
+    Checks if the user is currently an admin
+    If not, redirect to admin-login
+    """
+    @wraps(fn)
+    def go(*args, **kwargs):
+        if 'role' in session and session['role'] == 'Admin':
+        #if 'mod' in session:
+            return fn(*args, **kwargs)
+        else:
+            raise err.Forbidden('You must be an admin to see this page'
+                                )  # proper would be 401 Unauthorized
+    return go
+
+class LoginForm(Form):
+    username = StringField('username', validators=[validators.DataRequired()])
+    password = StringField('password', validators=[validators.DataRequired()])
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/index')
+@requires_roles(Role.STUDENT, Role.PROFESSOR, Role.ADMIN, Role.PARENT)
+def index():
+    return render_template('index.html', session=session)
 
 @app.route('/security')
 def sec():
     data = db.getAllSecurity()
     return render_template('secview.html', data=data)
 
-@app.route('/login', methods=['POST'])
-def login():
-    user = request.form.get('username')
-    passw = request.form.get('password')
-    if not user or not passw:
-        raise BadInput('Missing Username or Password')
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear() # wipes out the cookie
+    return redirect('/index')
+    #return render_template('logout.html', data=data)
 
-    data = db.login(user, passw)
-    if not data:
-        raise BadInput('Incorrect Username/Password')
-    return render_template('index.html', data=data)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        data = db.login(form.username.data, form.password.data)
+        if not data:
+            raise err.BadInput('Incorrect Username/Password')
+        session['username'] = data['username']
+        session['role'] = int(data['SecurityLevel'])
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
+
+
+@app.errorhandler(err.BadInput)
+def handle_badinput(error):
+    return render_template('error.html', error_message=error.message), 415
+
+
+@app.errorhandler(err.e404)
+def handle_e404(error):
+    return render_template('error.html', error_message=error.message), 404
+
+
+@app.errorhandler(err.BadMedia)
+def handle_badmedia(error):
+    return render_template('error.html', error_message=error.message), 415
+
+
+@app.errorhandler(err.PermDenied)
+def handle_permdenied(error):
+    return render_template('error.html', error_message=error.message), 550
+
+
+@app.errorhandler(err.Forbidden)
+def handle_Forbidden(error):
+    return render_template('error.html', error_message=error.message), 403
+
+
+@app.errorhandler(err.DNE)
+def handle_DNE(error):
+    return render_template('error.html', error_message=error.message), 404
